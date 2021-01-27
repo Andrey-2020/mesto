364c854c1cd58ed57afd55da6bb2ca19e4d23ab5
Андрей Медведев <andrenimedvedev@gmail.com>
2021-01-27 14:20:43 +0300

    feat: добавить popup добавления карточки, просмотра фото

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
 let popup = document.querySelector(".popup");
 let openButton = document.querySelector(".profile__button_edit");
 let closeButton = popup.querySelector(".popup__button");
 let togglePopup = () => {
     popup.classList.toggle("popup_opened")
 }
+
 let formElement = document.querySelector(".form")
 let profilename = document.querySelector(".profile__autor")
 let profilejob = document.querySelector(".profile__profession")
@@ -13,22 +42,15 @@ openButton.addEventListener("click", function () {
     togglePopup()
     nameInput.value = profilename.textContent
     jobInput.value = profilejob.textContent
-}
-)
+})
+
 closeButton.addEventListener("click", togglePopup)
 popup.addEventListener("click", (evt) => {
     if (evt.target === evt.currentTarget) {
         togglePopup()
     }
 })
-let likes = document.querySelectorAll(".place__like");
 
-likes.forEach(function (item) {
-    let toggleLike = () => {
-        item.classList.toggle("place__like_active")
-    }
-    item.addEventListener("click", toggleLike)
-});
 
 
 
@@ -53,3 +75,65 @@ function handleFormSubmit(evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+
+let addpopup = document.querySelector(".popup-add");
+let closeAddButton = addpopup.querySelector(".popup-add__button");
+let addButton = document.querySelector(".profile__button_add");
+let toggleAddPopup = () => {
+    addpopup.classList.toggle("popup_opened")
+}
+closeAddButton.addEventListener("click", toggleAddPopup)
+addButton.addEventListener("click", function () {
+    toggleAddPopup()
+
+})
+
+const places = document.querySelector(".places")
+const itemTemplate = document.querySelector(".item-template").content;
+
+initialCards.forEach((item) => {
+    renderCard(item.name, item.link)
+});
+let popupImg = document.querySelector(".popup-image");
+let closeImgButton = document.querySelector(".popup-image__button");
+let toggleImgPopup = () => {
+    popupImg.classList.toggle("popup_opened")
+}
+function renderCard(name, link) {
+    const htmlElement = itemTemplate.querySelector('.place').cloneNode(true);
+    let photo = htmlElement.querySelector(".place__image");
+    const like = htmlElement.querySelector('.place__like')
+    like.addEventListener("click", function () {
+        like.classList.toggle("place__like_active")
+    });
+    const deleteCard = htmlElement.querySelector('.place__delete')
+    deleteCard.addEventListener("click", function () {
+        const deleteItem = deleteCard.closest('.place')
+        deleteItem.remove()
+    });
+    photo.addEventListener("click", function () {
+        toggleImgPopup()
+        popupImg.querySelector('.popup__caption').textContent = name;
+        popupImg.querySelector(".popup__image").setAttribute('src', link);
+        popupImg.querySelector(".popup__image").setAttribute('alt', name);
+    })
+
+    htmlElement.querySelector('.place__title').textContent = name;
+    htmlElement.querySelector(".place__image").setAttribute('src', link);
+    htmlElement.querySelector(".place__image").setAttribute('alt', name);
+    places.append(htmlElement);
+
+}
+closeImgButton.addEventListener("click", toggleImgPopup)
+
+let formAddElement = document.querySelector(".form-add")
+function handleFormAddSubmit(evt) {
+    evt.preventDefault();
+    let nameAdd = formAddElement.querySelector(".form-add__item_type_name");
+    let link = formAddElement.querySelector(".form-add__item_type_link");
+    renderCard(nameAdd.value, link.value)
+    toggleAddPopup();
+
+}
+
+formAddElement.addEventListener('submit', handleFormAddSubmit);
