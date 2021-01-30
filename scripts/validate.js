d31f89c48be72f6240dfe06fc32f2184608cf6c3
Андрей Медведев <andrenimedvedev@gmail.com>
2021-01-30 22:42:14 +0300

    feat: сделать код более универсальным, через объект параметров

--- scripts/validate.js
+++ scripts/validate.js
@@ -12,7 +12,12 @@ function hideInputError(formElement, inputElement, parameters) {
 }
 function isValid(formElement, inputElement, parameters) {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage, parameters);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      parameters
+    );
   } else {
     hideInputError(formElement, inputElement, parameters);
   }
@@ -33,9 +38,13 @@ const toggleButtonState = (inputList, buttonElement, parameters) => {
 const setEventListeners = (formElement, parameters) => {
   // Находим все поля внутри формы,
   // сделаем из них массив методом Array.from
-  const InputList = Array.from(formElement.querySelectorAll(parameters.inputSelector))
+  const InputList = Array.from(
+    formElement.querySelectorAll(parameters.inputSelector)
+  );
   // Найдём в текущей форме кнопку отправки
-  const buttonElement = formElement.querySelector(parameters.submitButtonSelector);
+  const buttonElement = formElement.querySelector(
+    parameters.submitButtonSelector
+  );
   // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
   toggleButtonState(InputList, buttonElement, parameters);
   // Обойдём все элементы полученной коллекции
@@ -54,7 +63,9 @@ const setEventListeners = (formElement, parameters) => {
 function enableValidation(parameters) {
   // Найдём все формы с указанным классом в DOM,
   // сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll(parameters.formSelector));
+  const formList = Array.from(
+    document.querySelectorAll(parameters.formSelector)
+  );
 
   // Переберём полученную коллекцию
   formList.forEach((formElement) => {
@@ -71,10 +82,10 @@ function enableValidation(parameters) {
 
 // Вызовем функцию
 enableValidation({
-    formSelector: '.form',
-    inputSelector: '.form__input',
-    submitButtonSelector: '.form__button',
-    inactiveButtonClass: 'form__submit_inactive',
-    inputErrorClass: 'form__input_type_error', 
-    errorClass: 'form__input-error_active'
-  });
+  formSelector: ".form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".form__button",
+  inactiveButtonClass: "form__submit_inactive",
+  inputErrorClass: "form__input_type_error",
+  errorClass: "form__input-error_active",
+});
