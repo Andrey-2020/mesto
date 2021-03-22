de3401b9882f80035706df9d691bda0c24a76657
Андрей Медведев <andrei.medmedvedev@yandex.ru>
2021-03-22 23:57:12 +0300

    refactor: создание классов, дробление js на модули

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,86 +1,86 @@
-export default class FormValidator{
+export default class FormValidator {
   constructor(parameters, formSelector) {
     this._parameters = parameters
     this._formSelector = formSelector
-}
-_showInputError(formElement, inputElement, errorMessage) {
-  const formError = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(this._parameters.inputErrorClass);
-  formError.textContent = errorMessage;
-  formError.classList.add(this._parameters.errorClass);
-}
-_hideInputError(formElement, inputElement) {
-  const formError = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(this._parameters.inputErrorClass);
-  formError.classList.remove(this._parameters.errorClass);
-  formError.textContent = " ";
-}
-_isValid(formElement, inputElement) {
-  if (!inputElement.validity.valid) {
-    this._showInputError(
-      formElement,
-      inputElement,
-      inputElement.validationMessage
+  }
+  _showInputError(formElement, inputElement, errorMessage) {
+    const formError = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(this._parameters.inputErrorClass);
+    formError.textContent = errorMessage;
+    formError.classList.add(this._parameters.errorClass);
+  }
+  _hideInputError(formElement, inputElement) {
+    const formError = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(this._parameters.inputErrorClass);
+    formError.classList.remove(this._parameters.errorClass);
+    formError.textContent = " ";
+  }
+  _isValid(formElement, inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(
+        formElement,
+        inputElement,
+        inputElement.validationMessage
+      );
+    } else {
+      this._hideInputError(formElement, inputElement);
+    }
+  }
+  _hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  };
+  _toggleButtonState = (inputList, buttonElement) => {
+    if (this._hasInvalidInput(inputList)) {
+      buttonElement.classList.add(this._parameters.inactiveButtonClass);
+    } else {
+      buttonElement.classList.remove(this._parameters.inactiveButtonClass);
+    }
+  };
+  //setEventListeners добавит обработчики сразу всем полям формы
+  _setEventListeners = (formElement) => {
+    // Находим все поля внутри формы,
+    // сделаем из них массив методом Array.from
+    const InputList = Array.from(
+      formElement.querySelectorAll(this._parameters.inputSelector)
     );
-  } else {
-    this._hideInputError(formElement, inputElement);
-  }
-}
-_hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-_toggleButtonState = (inputList, buttonElement) => {
-  if (this._hasInvalidInput(inputList)) {
-    buttonElement.classList.add(this._parameters.inactiveButtonClass);
-  } else {
-    buttonElement.classList.remove(this._parameters.inactiveButtonClass);
-  }
-};
-//setEventListeners добавит обработчики сразу всем полям формы
-_setEventListeners = (formElement) => {
-  // Находим все поля внутри формы,
-  // сделаем из них массив методом Array.from
-  const InputList = Array.from(
-    formElement.querySelectorAll(this._parameters.inputSelector)
-  );
-  // Найдём в текущей форме кнопку отправки
-  const buttonElement = formElement.querySelector(
-    this._parameters.submitButtonSelector
-  );
-  // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
-  this._toggleButtonState(InputList, buttonElement);
-  // Обойдём все элементы полученной коллекции
-  InputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
-    inputElement.addEventListener("input", () => {
-      // Внутри колбэка вызовем isValid,
-      // передав ей форму и проверяемый элемент
-      this._isValid(formElement, inputElement);
-      this._toggleButtonState(InputList, buttonElement);
+    // Найдём в текущей форме кнопку отправки
+    const buttonElement = formElement.querySelector(
+      this._parameters.submitButtonSelector
+    );
+    // Вызовем toggleButtonState, чтобы не ждать ввода данных в поля
+    this._toggleButtonState(InputList, buttonElement);
+    // Обойдём все элементы полученной коллекции
+    InputList.forEach((inputElement) => {
+      // каждому полю добавим обработчик события input
+      inputElement.addEventListener("input", () => {
+        // Внутри колбэка вызовем isValid,
+        // передав ей форму и проверяемый элемент
+        this._isValid(formElement, inputElement);
+        this._toggleButtonState(InputList, buttonElement);
+      });
     });
-  });
-};
+  };
 
-//Добавление обработчиков всем формам
-enableValidation() {
-  // Найдём все формы с указанным классом в DOM,
-  // сделаем из них массив методом Array.from
-  const formList = Array.from(
-    document.querySelectorAll(this._formSelector)
-  );
+  //Добавление обработчиков всем формам
+  enableValidation() {
+    // Найдём все формы с указанным классом в DOM,
+    // сделаем из них массив методом Array.from
+    const formList = Array.from(
+      document.querySelectorAll(this._formSelector)
+    );
 
-  // Переберём полученную коллекцию
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", (evt) => {
-      // У каждой формы отменим стандартное поведение
-      evt.preventDefault();
+    // Переберём полученную коллекцию
+    formList.forEach((formElement) => {
+      formElement.addEventListener("submit", (evt) => {
+        // У каждой формы отменим стандартное поведение
+        evt.preventDefault();
+      });
+
+      // Для каждой формы вызовем функцию setEventListeners,
+      // передав ей элемент формы
+      this._setEventListeners(formElement);
     });
-
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
-    this._setEventListeners(formElement);
-  });
-}
+  }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -2,128 +2,127 @@ import Card from './Card.js';
 import FormValidator from './FormValidator.js';
 const initialCards = [
     {
-      name: "Архыз",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+        name: "Архыз",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
     {
-      name: "Челябинская область",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+        name: "Челябинская область",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
     },
     {
-      name: "Иваново",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+        name: "Иваново",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
     },
     {
-      name: "Камчатка",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+        name: "Камчатка",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
     {
-      name: "Холмогорский район",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+        name: "Холмогорский район",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
     },
     {
-      name: "Байкал",
-      link:
-        "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+        name: "Байкал",
+        link:
+            "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     },
-  ];
-  const popup = document.querySelector(".popup_type_edit");
-  const openButton = document.querySelector(".profile__button_edit");
-  export const places = document.querySelector(".places");
-  const formElement = document.querySelector(".form_type_edit");
-  const profilename = document.querySelector(".profile__autor");
-  const profilejob = document.querySelector(".profile__profession");
-  const nameInput = formElement.querySelector(".form__input_type_name-input");
-  const jobInput = formElement.querySelector(".form__input_type_job-input");
-  const addpopup = document.querySelector(".popup_type_add");
-  const closeAddButton = addpopup.querySelector(".popup__button");
-  const addButton = document.querySelector(".profile__button_add");
-  export const popupImg = document.querySelector(".popup_type_image");
-  const popups = document.querySelectorAll(".popup");
+];
+const popup = document.querySelector(".popup_type_edit");
+const openButton = document.querySelector(".profile__button_edit");
+export const places = document.querySelector(".places");
+const formElement = document.querySelector(".form_type_edit");
+const profilename = document.querySelector(".profile__autor");
+const profilejob = document.querySelector(".profile__profession");
+const nameInput = formElement.querySelector(".form__input_type_name-input");
+const jobInput = formElement.querySelector(".form__input_type_job-input");
+const addpopup = document.querySelector(".popup_type_add");
+const closeAddButton = addpopup.querySelector(".popup__button");
+const addButton = document.querySelector(".profile__button_add");
+export const popupImg = document.querySelector(".popup_type_image");
+const popups = document.querySelectorAll(".popup");
 
 function openModal(popup) {
     popup.classList.add("popup_opened");
     document.addEventListener("keydown", closeByEscape);
-  }
-  function closeModal(popup) {
+}
+function closeModal(popup) {
     popup.classList.remove("popup_opened");
     document.removeEventListener("keydown", closeByEscape);
-  }
-  function closeByEscape(evt) {
+}
+function closeByEscape(evt) {
     if (evt.key === "Escape") {
-      const openedPopup = document.querySelector(".popup_opened");
-      closeModal(openedPopup);
+        const openedPopup = document.querySelector(".popup_opened");
+        closeModal(openedPopup);
     }
-  }
-  
-  
-  openButton.addEventListener("click", function () {
+}
+
+
+openButton.addEventListener("click", function () {
     openModal(popup);
     nameInput.value = profilename.textContent;
     jobInput.value = profilejob.textContent;
-  });
-  
-  
-  popups.forEach((popup) => {
+});
+
+
+popups.forEach((popup) => {
     popup.addEventListener("click", (evt) => {
-      if (evt.target.classList.contains("popup_opened")) {
-        closeModal(popup);
-      }
-      if (evt.target.classList.contains("popup__button")) {
-        closeModal(popup);
-      }
+        if (evt.target.classList.contains("popup_opened")) {
+            closeModal(popup);
+        }
+        if (evt.target.classList.contains("popup__button")) {
+            closeModal(popup);
+        }
     });
-  });
-  
-  function handleFormSubmit(evt) {
+});
+
+function handleFormSubmit(evt) {
     evt.preventDefault();
     profilename.textContent = nameInput.value;
     profilejob.textContent = jobInput.value;
-  
+
     closeModal(popup);
-  }
-  
-  formElement.addEventListener("submit", handleFormSubmit);
-  
-  
-  closeAddButton.addEventListener("click", function () {
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
+
+
+closeAddButton.addEventListener("click", function () {
     closeModal(addpopup);
-  });
-  addButton.addEventListener("click", function () {
+});
+addButton.addEventListener("click", function () {
     openModal(addpopup);
-  });
-  
-  initialCards.reverse().forEach((item) => {
+});
+
+initialCards.reverse().forEach((item) => {
     const card = new Card(item.name, item.link);
     card.renderCard()
-  });
-  
-  const formAddElement = document.querySelector(".form_type_add");
-  const nameAdd = formAddElement.querySelector(".form__input_type_name");
-  const link = formAddElement.querySelector(".form__input_type_link");
-  
-  function handleFormAddCardSubmit(evt) {
+});
+
+const formAddElement = document.querySelector(".form_type_add");
+const nameAdd = formAddElement.querySelector(".form__input_type_name");
+const link = formAddElement.querySelector(".form__input_type_link");
+
+function handleFormAddCardSubmit(evt) {
     evt.preventDefault();
     const card = new Card(nameAdd.value, link.value);
     card.renderCard()
     closeModal(addpopup);
-  }
-  
-  formAddElement.addEventListener("submit", handleFormAddCardSubmit);
+}
 
-  const Valid =new FormValidator({
+formAddElement.addEventListener("submit", handleFormAddCardSubmit);
+
+const Valid = new FormValidator({
     inputSelector: ".form__input",
     submitButtonSelector: ".form__button",
     inactiveButtonClass: "form__button_inactive",
     inputErrorClass: "form__input_type_error",
     errorClass: "form__input-error_active",
-  }, ".form")
-  // Вызовем функцию
+}, ".form")
+// Вызовем функцию
 
-  Valid.enableValidation()
-  
+Valid.enableValidation()
